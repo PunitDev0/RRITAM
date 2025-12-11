75c0db2352ddc5cb5f6df64776be8fdcca5cfd85
PunitDev0 <punitdeveloper1@gmail.com>
2025-12-11 17:34:49 +0530

    update timing

--- public/build/assets/table-B1QbZeP7.js
+++ public/build/assets/table-B1QbZeP7.js
@@ -1,4 +1,4 @@
-import{a as l,c as o}from"./createLucideIcon-DK-qEKxp.js";import{j as t}from"./app-CVWWDt1s.js";/**
+import{a as l,c as o}from"./createLucideIcon-BzKt5iSb.js";import{j as t}from"./app-ClRN3Yc7.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
